fda4db53ae7782bd66cc8cd08f2b7b238dc9390f
AnubioG <rubioporrasangela@gmail.com>
2024-09-18 10:28:29 -0500

    Back to categories  button

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -1,16 +1,28 @@
 import { useRecipesDetail } from "@/hooks/useRecipeDetail";
+import { useRecipeStore } from "@/store/store";
 
 export function RecipeDetail() {
+  const { setCurretId } = useRecipeStore();
   const { recipe, isLoading } = useRecipesDetail();
+
+  const handleClick = () => {
+    setCurretId(0);
+  };
+
   return (
     <>
       {isLoading ? (
         <p>Cargando receta</p>
       ) : (
         <div className="flex flex-col items-center  my-8 mx-8 sm:mx-16">
-          <div className="flex w-full justify-between">
-            <div>HomeChef</div>
-            <button>Regresar a categorias</button>
+          <div className="flex w-full justify-between items-center">
+            <div className="text-xs">HomeChef</div>
+            <button
+              onClick={handleClick}
+              className="py-3 px-5 bg-white text-blue-950 rounded-full font-bold"
+            >
+              Back to categories
+            </button>
           </div>
           <div className="flex flex-col items-start justify-center mt-8 gap-5 w-3/5 max-w-xl ">
             <img
